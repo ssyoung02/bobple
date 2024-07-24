7ec8933bd5e139908518202d8442edd3cc13295a
ohmintaeak <omt610@gmail.com>
2024-07-24 15:22:32 +0900

    메인 탭 페이지 기본설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import './App.css';
+import MainPage from "./components/MainPage";
+import Login from "./components/Login";
+import RecipeMain from "./components/Recipe/RecipeMain";
+import RecommendMain from "./components/RecommendFood/RecommendMain"
+import AroundMain from "./components/AroundFood/AroundMain";
+import GroupMain from "./components/EatingGroup/GroupMain";
+import MyInfoMain from "./components/MyPage/MyInfoMain";
+import PointMain from "./components/PointShop/PointMain"
 
 function App() {
     return (
-        <div>
-            <h1>hi</h1>
-            <h2>react</h2>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<MainPage/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/recipe" element={<RecipeMain/>}/>
+                <Route path="/recommend" element={<RecommendMain/>}/>
+                <Route path="/around" element={<AroundMain/>}/>
+                <Route path="/group" element={<GroupMain/>}/>
+                <Route path="/myinfo" element={<MyInfoMain/>}/>
+                <Route path="/point" element={<PointMain/>}/>
+            </Routes>
+        </Router>
     );
 }
 
--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,1 +1,44 @@
-// 한 페이지당 컴포넌트 폴더안에서 파일 추가할 것.
+import React from "react";
+import {useNavigate} from 'react-router-dom';
+import '../css/MainPage.css'
+
+function MainPage() {
+    const navigate = useNavigate();
+
+    const moveRecipe = () => {
+        navigate('/Recipe');
+    }
+
+    const moveRecommend = () => {
+        navigate('/RecommendFood');
+    }
+
+    const moveAround = () => {
+        navigate('/Recipe');
+    }
+
+    const moveGroup = () => {
+        navigate('/RecommendFood');
+    }
+
+    const moveMyinfo = () => {
+        navigate('/Recipe');
+    }
+
+    const movePoint = () => {
+        navigate('/RecommendFood');
+    }
+
+    return (
+        <div className="footer-buttons">
+            <button className="recipe-button footer-btn" onClick={moveRecipe}>레시피</button>
+            <button className="recommend-button footer-btn" onClick={moveRecommend}>맛집추천</button>
+            <button className="around-button footer-btn" onClick={moveAround}>주변맛집</button>
+            <button className="group-button footer-btn" onClick={moveGroup}>함께먹기</button>
+            <button className="myinfo-button footer-btn" onClick={moveMyinfo}>내정보</button>
+            <button className="point-button footer-btn" onClick={movePoint}>포인트샵</button>
+        </div>
+    )
+}
+
+export default MainPage;
