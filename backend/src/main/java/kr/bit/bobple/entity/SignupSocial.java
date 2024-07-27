fee3e8714c286b30accddb2fba51dfe4da1b9367
yujung <148977734+yujung1339@users.noreply.github.com>
2024-07-27 15:27:23 +0900

    카카오 소셜 로그인 인가코드 받기

---
+++ backend/src/main/java/kr/bit/bobple/entity/SignupSocial.java
@@ -0,0 +1,13 @@
+//package kr.bit.bobple.entity;
+//
+//import lombok.*;
+//
+//@Builder
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class SignupSocial {
+//    private String token;
+//    private Long userId;
+//    private User user;
+//}
