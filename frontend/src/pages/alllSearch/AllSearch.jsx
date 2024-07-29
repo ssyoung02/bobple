d68695484d867025df4358b803d9fe02f6066abd
jihyeon <ilove3531@naver.com>
2024-07-29 18:20:18 +0900

    검색 페이지 검색바 생성

--- frontend/src/pages/alllSearch/AllSearch.jsx
+++ frontend/src/pages/alllSearch/AllSearch.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import '../../assets/style/allSearch/AllSearch.css'
 
 function AllSearch(){
 
     return(
         <div className={"SearchBox"}>
-            <div>
-
+            <div className={"SearchInput"}>
+                <input className={"AllSaerchBox"} placeholder={"검색 키워드를 입력해주세요"}/>
+                <button className={"AllSearchButton"}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
             </div>
         </div>
 
