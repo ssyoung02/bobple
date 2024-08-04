a94bbf309a76a2cff45b58afbc9c59f229e65461
ohmintaeak <omt610@gmail.com>
2024-08-04 23:44:00 +0900

    관리자 페이지 라우터 수정(레이아웃 제거) 및 정보 테이블폼 작업중

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import MainRouter from './router/MainRouter';
 import './App.css';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
+import AdminRouter from "./router/AdminRouter";
 
 function App() {
     return (
         <BrowserRouter>
-            <Layout>
-                <div className="App">
-                    <MainRouter />
-                </div>
-            </Layout>
+            <div className="App">
+                <Routes>
+                    <Route path="/admin/*" element={<AdminRouter />} />
+                    <Route path="/*" element={
+                        <Layout>
+                            <MainRouter />
+                        </Layout>
+                    } />
+                </Routes>
+            </div>
         </BrowserRouter>
     );
 }
