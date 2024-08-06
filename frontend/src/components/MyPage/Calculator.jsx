e9e2cccc8cb450ce7752e0af16ae4fcb5fd061a0
ohmintaeak <omt610@gmail.com>
2024-08-06 15:21:42 +0900

    음식 이상형 월드컵 완성(추후 디자인 수정)

--- frontend/src/components/MyPage/Calculator.jsx
+++ frontend/src/components/MyPage/Calculator.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-function Calculaton() {
+function Calculator() {
     const [file, setFile] = useState(null);
     const [ocrResponse, setOcrResponse] = useState(null);
     const [totalAmount, setTotalAmount] = useState('');
--- frontend/src/pages/recommendFood/foodWorldCup/FoodWorldCup.jsx
+++ frontend/src/pages/recommendFood/foodWorldCup/FoodWorldCup.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import {ArrowLeftLong, Trophy} from "../../../components/imgcomponents/ImgComponents";
+import '../../../assets/style/recommendFood/FoodWorldCup.css'
+import {useNavigate} from "react-router-dom";
 
 function FoodWorldCup() {
+    const navigate = useNavigate();
+
+    const moveRecommend = () => {
+        navigate('/recommend');
+    }
+
+    const moveWorldCup = () => {
+        navigate('/recommend/foodWorldCup/foodWorldCupGame')
+    }
+
     return(
-        <>
-        </>
+        <div className="worldCup-container">
+            <div className="worldCup-header">
+                <button className="arrow-btn" onClick={moveRecommend}>
+                    <ArrowLeftLong/>
+                </button>
+                <h2 className="worldCup-title">Food World Cup</h2>
+            </div>
+            <Trophy/>
+            <p className="worldCup-text">
+                월드컵은 16강으로 진행됩니다.<br/>
+                랜덤으로 음식이 나오니<br/>
+                메뉴가 고민이신 분들은<br/>
+                게임을  통해 정해보세요!</p>
+            <button className="worldCup-btn" onClick={moveWorldCup}>START! ➡️</button>
+        </div>
     );
 }
 
