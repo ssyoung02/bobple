4ad4db48be03a2621a66bc89ae3079b2cb1ba213
ohmintaeak <omt610@gmail.com>
2024-08-13 15:39:09 +0900

    페이지 다크모드 공사중..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import MainRouter from './router/MainRouter';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -6,12 +6,23 @@ import Layout from "./components/layout/Layout";
 import AdminRouter from "./router/AdminRouter";
 
 function App() {
+
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    };
+
+    useEffect(() => {
+        document.body.className = theme === 'light' ? 'light-mode' : 'dark-mode';
+    }, [theme]);
+
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/admin/*" element={<AdminRouter />} />
                 <Route path="/*" element={
-                    <Layout>
+                    <Layout theme={theme} toggleTheme={toggleTheme}>
                         <MainRouter />
                     </Layout>
                 } />
